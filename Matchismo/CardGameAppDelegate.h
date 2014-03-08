56aa7919b42284d2a7966265b43d1bd0cb12f8da
Philippe Targino <ptargino@Targino-Mac.local>
2014-03-08 17:55:13 -0300

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by Philippe Targino on 3/8/14.
+//  Copyright (c) 2014 Philippe Targino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
